376728a66649188c30e2f27e361af2672c29354c
LiuSS8080 <871243720@qq.com>
2019-12-19 14:11:21 +0800

    整合

--- src/main/java/com/woniu/util/Solution.java
+++ src/main/java/com/woniu/util/Solution.java
@@ -5,23 +5,59 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
-public class Solution {
+public class Solution {	/**
+	 * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+	 * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+	 * 给定 nums = [2, 7, 11, 15], target = 9
+	 * 因为 nums[0] + nums[1] = 2 + 7 = 9
+	 * 所以返回 [0, 1]
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
 	public int[] twoSum(int[] nums, int target) {
-		Map<Integer, Integer> m = new HashMap<Integer, Integer>();
-		int[] arr = new int[2];
+		int[] result = new int[2];
 		for (int i = 0; i < nums.length; i++) {
-			if (m.containsKey(nums[i])) {
-				nums[0] = m.get(target - nums[i]);
-				nums[1] = i;
+			for (int j = i+1; j < nums.length; j++) {
+				if(nums[i]+nums[j]==target){
+					result[0] = i;
+					result[1] = j;
+				}
 			}
-			m.put(target - nums[i], i);
 		}
-
-		return arr;
-
-	}
+		return result;
+    }
+	public int[] twoSum2(int[] nums, int target) {
+		int[] result = new int[2];
+		Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			if(map.containsKey(nums[i])){
+				result[0] = map.get(new Integer(1));
+				result[1] = map.get(1);
+				break;
+			}
+			map.put(target-nums[i], i);
+			
+		}
+		return result;
+    }
+	/**
+	 * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+	 * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用O(1) 的额外空间解决这一问题。
+	 * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+	 */
+	 public void reverseString(char[] s) {
+		 Character temp = null;
+		 for (int i = 0; i < s.length/2; i++) {
+			temp = s[i];
+			s[i] = s[s.length-1-i];
+			s[s.length-1-i] = temp;
+		} 
+	 }
+	
+	
+	
 	public int minTimeToVisitAllPoints(int[][] points) {
         int time = 0;
         int x = 0;
