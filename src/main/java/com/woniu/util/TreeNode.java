08fc1b0370b79affd20a024eaac254e022bad35c
liusongsong <871243720@qq.com>
2020-01-14 22:46:09 +0800

    20200113 2道

--- src/main/java/com/woniu/util/TreeNode.java
+++ src/main/java/com/woniu/util/TreeNode.java
@@ -2,8 +2,8 @@ package com.woniu.util;
 
 public class TreeNode {
     int val;
-    s20200114.TreeNode left;
-    s20200114.TreeNode right;
+    TreeNode left;
+    TreeNode right;
     TreeNode(int x) {
         val = x;
     }
